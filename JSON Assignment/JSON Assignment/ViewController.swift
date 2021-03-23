812bf6323a2bd5f516bb652f362871315bb83040
Mohamad Nehme <mohamadnehme98@hotmail.com>
2021-03-23 13:55:25 +0200

    initial commit

---
+++ JSON Assignment/JSON Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON Assignment
+//
+//  Created by Mohamad Nehme on 23/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
